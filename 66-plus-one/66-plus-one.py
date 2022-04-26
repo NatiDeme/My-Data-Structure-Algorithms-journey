efe599719e7404de80353e533baf8846889a621a
Natnael Demelash <demelashnatnael@gmail.com>
2022-04-26 05:52:33 +0300

    Time: 43 ms (5.62%), Space: 13.2 MB (92.00%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] <9:
+                digits[i]+=1
+                return digits
+            else:
+                digits[i]=0
+        
+        digits.insert(0,1)
+        return digits
+        
+      
+        
